f7069291aed637b7b7dc627f05ea79a85c656f4d
Alex Hall <james_a_hall@homedepot.com>
2019-06-04 10:57:14 -0400

    Use nodemon instead of raw node for dev

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const debug = require('debug')('app');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
@@ -34,4 +35,7 @@ app.use((err, req, res, next) => {
   res.render('error');
 });
 
-module.exports = app;
+app.set('port', process.env.PORT || 3002);
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
