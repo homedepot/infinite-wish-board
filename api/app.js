ccf4bb99ae0fb1677901ced3df717ebc8c4354dc
Alex Hall <me@jalexhall.com>
2019-06-07 18:23:36 -0400

    Add gzip

--- api/app.js
+++ api/app.js
@@ -3,12 +3,15 @@ const logger = require('morgan');
 const debug = require('debug')('app');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const compression = require('compression')
+
 
 const index = require('./routes/index');
 const users = require('./routes/users');
 
 const app = express();
 
+app.use(compression())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
