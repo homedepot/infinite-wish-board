e04680aaa5b23cda6d3ad5a37107a23b22fa9dee
Alex Hall <me@jalexhall.com>
2019-06-08 20:39:42 -0400

    Prettier format

--- ui/src/app/App.test.js
+++ ui/src/app/App.test.js
@@ -1,12 +1,12 @@
-import React from "react"
-import { shallow } from "enzyme"
-import App from "./App"
-import { Route } from "react-router-dom"
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
+import { Route } from 'react-router-dom'
 
-describe("Default routing behavior", () => {
-  it("renders the landing page by default", () => {
+describe('Default routing behavior', () => {
+  it('renders the login page by default', () => {
     const wrapper = shallow(<App />)
 
-    expect(wrapper.find(Route).props().path).toEqual("/")
+    expect(wrapper.find(Route).props().path).toEqual('/')
   })
 })
