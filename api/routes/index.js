3623ce2725e9be3abbfb7951adea6b388d82252a
Alex Hall <me@jalexhall.com>
2019-06-08 20:40:31 -0400

    Prettier format

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.send('hello')
-});
+})
 
-module.exports = router;
+module.exports = router
