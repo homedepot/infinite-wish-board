938e6ad2b75230d12907b5a8660dd72cc32a477b
Alex Hall <me@jalexhall.com>
2019-06-08 20:40:45 -0400

    Return 200 instead of redirect, prettier format

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,27 +1,32 @@
-const router = require("express").Router()
-const passport = require("passport")
-const Account = require("./../db/Account")
+const router = require('express').Router()
+const passport = require('passport')
+const Account = require('./../db/Account')
 
-router.post("/register", function(req, res, next) {
-  console.log("registering user")
+router.post('/register', function(req, res, next) {
+  console.log('registering user')
   Account.register(
     new Account({ username: req.body.username }),
     req.body.password,
     function(err) {
       if (err) {
-        console.log("error while user register!", err)
+        console.log('error while user register!', err)
         return next(err)
       }
 
-      console.log("user registered!")
+      console.log('user registered!')
 
-      res.redirect("/")
+      res.sendStatus(200)
     }
   )
 })
 
-router.post("/login", passport.authenticate("local"), function(req, res) {
-  res.redirect("/")
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.sendStatus(200)
+})
+
+router.get('/logout', function(req, res) {
+  req.logout()
+  res.sendStatus(200)
 })
 
 module.exports = router
