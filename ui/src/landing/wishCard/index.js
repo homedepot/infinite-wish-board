f218762248c937a52a48081ed031617ae91ff993
Connor Jones <connorjones135@gmail.com>
2019-06-14 14:14:49 -0400

    Splash screen (#3)
    * styling splash screen
    
    * styling splash screen
    
    * Add CSS to the splash screen
    
    * Add test cases for splash screen and components

---
+++ ui/src/landing/wishCard/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.scss'
+
+export const WishCard = ({imgSrc, altText, title, subtitle}) => (
+  <div id="WishCard" className="containerVertical">
+    <img src={imgSrc} alt={altText} className="wish-card-image"/>
+    <span className="card-title">{title}</span>
+    <span className="card-subtitle">{subtitle}</span>
+  </div>
+)
