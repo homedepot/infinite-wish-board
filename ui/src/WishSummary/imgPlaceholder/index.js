5634027f28eb9aa3f8d0c6cd2006483725b00d20
Aswin Kumar Alapati <aswin_k_alapati@homedepot.com>
2019-06-14 17:52:39 -0400

    fixed eslint errors

--- ui/src/WishSummary/imgPlaceholder/index.js
+++ ui/src/WishSummary/imgPlaceholder/index.js
@@ -4,15 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera } from '@fortawesome/free-solid-svg-icons'
 
 export default class ImgPlaceholder extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (
             <div className="icon-container">
                 <FontAwesomeIcon className="camera-icon" icon={faCamera} />
-                <a href="#">Add Image</a>
+                <a href="/review">Add Image</a>
             </div>
         )
     }
