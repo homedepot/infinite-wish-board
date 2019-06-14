f37cd3f116321395d85a68da334b2fb4ddc52db2
Li Ding <li_ding@homedepot.com>
2019-06-14 13:32:49 -0400

    update tests for next step button

--- ui/src/childinfo/ChildInfo.test.js
+++ ui/src/childinfo/ChildInfo.test.js
@@ -4,9 +4,11 @@ import { shallow } from 'enzyme'
 
 // noinspection JSAnnotator
 describe('Initial Render', () => {
-  let childInfo
-  beforeEach( () => {
-    childInfo = shallow(<ChildInfo />)
+  window.scrollTo = jest.fn(() => {});
+
+  let childInfo;
+  beforeEach(() => {
+    childInfo = shallow(<ChildInfo />);
   })
 
   it('renders!', () => {
@@ -17,10 +19,30 @@ describe('Initial Render', () => {
 
   })
 
-  it('stepUpdate!', () => {
+  it('Should update `step` in state', () => {
     expect(childInfo.instance().state.step).toEqual(0);
     childInfo.instance().nextStep();
     expect(childInfo.instance().state.step).toEqual(1);
+    childInfo.instance().nextStep();
+    expect(childInfo.instance().state.step).toEqual(2);
+  })
 
+  describe('When user click next button', () => {
+
+    it('Should update text on button click', () => {
+      let nextButton = childInfo.find('.next-button');
+
+      expect(childInfo.find('.text-name').text()).toEqual('Hi what is your name?');
+      nextButton.simulate('click');
+      expect(childInfo.find('.text-name').text()).toEqual('Hi , how old are you?');
+      nextButton.simulate('click');
+      expect(childInfo.find('.text-name').text()).toEqual('Where are you from?');
+      nextButton.simulate('click');
+      expect(childInfo.find('.text-name').text()).toEqual('Tell us about your condition');
+      nextButton.simulate('click');
+      expect(childInfo.find('.text-name').text()).toEqual('Tell us more about your wish!');
+      nextButton.simulate('click');
+      expect(childInfo.find('.text-name').text()).toEqual('Tell us more about your wish!');
+    })
   })
 })
