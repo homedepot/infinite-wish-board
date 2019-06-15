d84df2e1f6f054939116416d16907dc2af73c8ac
Connor Jones <connorjones135@gmail.com>
2019-06-15 10:09:42 -0400

    WIP: refactoring, pulling wish data

--- ui/src/WishSummary/wish/index.js
+++ ui/src/WishSummary/wish/index.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react'
-import './styles.scss';
-import chevronRight from '../../assets/Chevron_Forward.png';
+import './styles.scss'
 import kidImg from '../../assets/icn_To_Be_Astronaut_130x130.png'
 import sponsorImg from '../../assets/icn_To_Be_Astronaut_130x130.png'
-import ImgPlaceholder from '../imgPlaceholder';
+import ImgPlaceholder from '../imgPlaceholder'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 export default class Wish extends Component {
-    render() {
-        return (
-            <div className="wish">
-                <div className="date">
-                </div>
-                {false ? <img src={kidImg} alt="child" /> :<ImgPlaceholder text="Add Image" />}
-                
-                <div>
-                    <p>
-                        <strong>Maggie</strong> - Age 6 from Smyrna
-                    </p>
-                    <span className="summary">
+  render() {
+    return (
+      <div className="wish">
+        <div className="date">
+        </div>
+        {false ? <img src={kidImg} alt="child" /> : <ImgPlaceholder text="Add Image" />}
+
+        <div>
+          <p>
+            <strong>Maggie</strong> - Age 6 from Smyrna
+          </p>
+          <span className="summary">
                         Maggie wishes to go to Disney land
                     </span>
-                </div>
-                {false ? <img src={sponsorImg} alt="Sponsor" className="sponsor" /> : <a href="/review">Add Sponsor</a>}
-                <img src={chevronRight} alt="click to view wish details" className="chevron"/>
-            </div>
-        )
-    }
+        </div>
+        {false ? <img src={sponsorImg} alt="Sponsor" className="sponsor" /> : <a href="/review">Add Sponsor</a>}
+        <FontAwesomeIcon icon={faChevronRight} className="wish-chevron" />
+      </div>
+    )
+  }
 }
--- ui/src/WishSummary/wishHeader/index.js
+++ ui/src/WishSummary/wishHeader/index.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import makeAWishLogo from '../../assets/Logo_MakeWish.png'
 import galaxyLogo from '../../assets/Logo_Galaxy.png'
 import './styles.scss'
 
-export default class WishHeader extends Component {
-    render() {
-        return (
-            <div id="wishHeader">
-                <div className="logo-container">
-                    <img src={makeAWishLogo} className="makeAWishLogo" alt="make a wish logo" />
-                    <img src={galaxyLogo} className="galaxyLogo" alt="galaxy logo" />
-                </div>
-                <div className="user-details">
-                    <span>Mary</span> | 
-                    <a href="/logout">Sign Out</a>
-                </div>
-            </div>
-        )
-    }
-}
+const WishHeader = () => (
+  <div id="wishHeader">
+    <div className="logo-container">
+      <img src={makeAWishLogo} className="makeAWishLogo" alt="make a wish logo" />
+      <img src={galaxyLogo} className="galaxyLogo" alt="galaxy logo" />
+    </div>
+    <div className="user-details">
+      <span>Mary</span> |
+      <a href="/logout">Sign Out</a>
+    </div>
+  </div>
+)
+
+export default WishHeader
