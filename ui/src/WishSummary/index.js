861d36dc9fcfff0db2197504045df3b476084a73
Connor Jones <connorjones135@gmail.com>
2019-06-15 10:09:42 -0400

    WIP: refactoring, pulling wish data

--- ui/src/WishSummary/index.js
+++ ui/src/WishSummary/index.js
@@ -1,20 +1,34 @@
-import React, { Component } from 'react';
-import Wish from './wish';
-import WishHeader from './wishHeader';
-import WishFilter from './wishFilter';
+import React, { Component } from 'react'
+import Wish from './wish'
+import WishHeader from './wishHeader'
+import WishFilter from './wishFilter'
 import './styles.scss'
+import { getWishes } from '../services/WishDetailsService'
 
 export default class WishSummary extends Component {
 
-    render() {
-        return (
-            <div className="wishSummary">
-                <WishHeader />
-                <WishFilter />
-                <Wish />
-                <Wish />
-                <Wish />
-            </div>
-        )
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      wishes: []
     }
+  }
+
+  componentDidMount = async () => {
+    const wishes = await getWishes()
+    this.setState({ wishes })
+  }
+
+  render() {
+    return (
+      <div id="WishSummary">
+        <WishHeader />
+        <WishFilter />
+        <Wish />
+        <Wish />
+        <Wish />
+      </div>
+    )
+  }
 }
