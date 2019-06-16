734efa42dc54bd6225e46d40ae7a707f64a639b0
Aswin A <aswin1689@gmail.com>
2019-06-16 14:28:10 -0400

    deleted unused images

--- ui/src/wishList/wish/index.js
+++ ui/src/wishList/wish/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import './styles.scss'
-import kidImg from '../../assets/images/icn_To_Be_Astronaut_130x130.png'
-import sponsorImg from '../../assets/images/icn_To_Be_Astronaut_130x130.png'
 import ImgPlaceholder from '../imgPlaceholder'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
