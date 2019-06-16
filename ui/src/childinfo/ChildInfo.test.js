7e17aedfb3701c1682d469a1051488c1e1cd3fb1
Li Ding <8593157+liding987@users.noreply.github.com>
2019-06-16 12:01:47 -0400

    Feature/child info page (#19)
    * WIP: initial commit of childInfo page
    
    * WIP: updated to ChildInfo
    
    * smooth scrolling
    
    * update tests for next step button
    
    * WIP: confirmation page
    
    * WIP: initial commit of childInfo page
    
    * WIP: updated to ChildInfo
    
    * smooth scrolling
    
    * update tests for next step button
    
    * WIP: confirmation page
    
    * WIP: rocket
    
    * Added rocket blast off animation
    
    * added initial services, created wishDetails
    
    * rocket blast off
    
    * update rocket
    
    * update show confirmation page logic
    
    * WIP: wish details page
    
    * Added make a wish API request
    
    * added dynamic images for wish type, fixed some tests + refactoring
    
    * fixed merge conflict
    
    * fixed destructured state, changes to async/await over .then syntax
    
    * fixed merge conflict
    
    * fixed one more merge conflict
    
    * make a post request of child info when the child blasts off the rocket
    
    * show wish details component in child info page after the rocket blast off
    
    * update test
    
    * mock service calls
    
    * get type from props
    
    * update test for showing wish details
    
    * resolve merge conflict
    
    * handled some edge cases and tests
    
    * destructure variables from state
    
    * update test
    
    * update test
    
    * add a test
    
    * resolve merge conflict
    
    * update eqeq to eqeqeq
    
    * unskip two tests
    
    * update a test
    
    * remove debug and console log

--- ui/src/childinfo/ChildInfo.test.js
+++ ui/src/childinfo/ChildInfo.test.js
@@ -2,14 +2,44 @@ import React from 'react'
 import ChildInfo from './ChildInfo'
 import { shallow } from 'enzyme'
 
+jest.mock('../services/WishDetailsService', () => ({
+  makeAWish: jest.fn(() => {
+    return ({
+      _id: 'mock child id'
+    })
+  }),
+  getWishDetails: jest.fn(() => {
+    return ({
+      id: '',
+      child: {
+        firstName: '',
+        lastName: ' ',
+        hometown: '',
+        illness: '',
+        age: ''
+      },
+      type: '',
+      details: '',
+      sponsor: {
+        name: '',
+        logo: '',
+        links: []
+      },
+      createdAt: '',
+      updatedAt: ''
+    })
+  })
+}));
+
 // noinspection JSAnnotator
 describe('Initial Render', () => {
-  window.scrollTo = jest.fn(() => {});
+  window.scrollTo = jest.fn(() => { });
   const testName = 'John'
   const age = 5;
   let childInfo;
+  let childId = 'some id string'
   beforeEach(() => {
-    childInfo = shallow(<ChildInfo name={testName} age={age}/>);
+    childInfo = shallow(<ChildInfo name={testName} age={age} />);
   })
 
   it('renders!', () => {
@@ -49,40 +79,62 @@ describe('Initial Render', () => {
 
   describe('When user click next button', () => {
     beforeEach(() => {
-      childInfo = shallow(<ChildInfo name={testName} age={age}/>);
+      childInfo = shallow(<ChildInfo name={testName} age={age} />);
     })
 
-    it('Should update text on button click', () => {
+    it('Should update text on button click', async () => {
       let nextButton = childInfo.find('.next-button');
       expect(childInfo.find('.text-name').text()).toEqual(`Hi ${testName}, I have a few questions for you before we can make your wish come true!`);
-      nextButton.simulate('click');
+      await nextButton.simulate('click');
       expect(childInfo.find('.text-name').text()).toEqual('Where are you from?');
-      nextButton.simulate('click');
+      await nextButton.simulate('click');
       expect(childInfo.find('.text-name').text()).toEqual('Tell us about your condition');
-      nextButton.simulate('click');
+      await nextButton.simulate('click');
       expect(childInfo.find('.text-name').text()).toEqual('Tell us more about your wish!');
-      nextButton.simulate('click');
+      await nextButton.simulate('click');
+
       expect(childInfo.find('.text-name').length).toEqual(0);
       expect(childInfo.instance().state.showConfirmation).toBeTruthy();
+    })
+  })
+
+  describe('When `showWishDetails` in state is true and `childId` in state is defined', () => {
+    beforeEach(() => {
+      childInfo = shallow(<ChildInfo />);
+      childInfo.instance().setState({
+        showWishDetails: true,
+        childId: 'a child id'
+      })
+    })
+
+    it('Should show WishDetails component', () => {
+      expect(childInfo.find('WishDetails').length).toEqual(1)
     })
   })
 
   describe('When user on confirmation page and click blast off the rocket', () => {
 
     beforeEach(() => {
-      childInfo = shallow(<ChildInfo name={testName} age={age}/>);
+      childInfo = shallow(<ChildInfo name={testName} age={age} />);
       childInfo.instance().setState({
         showConfirmation: true,
         isBlastOff: false
       })
     })
 
-    it('Should update the `rocketRotation` and `rocketWidth` in state', () => {
-      childInfo.find('button').simulate('click')
+    it('Should disapear', () => {
+      expect(childInfo.find('.rocket-blast-off-button').length).toEqual(1)
+      childInfo.find('.rocket-blast-off-button').simulate('click')
+      expect(childInfo.find('.rocket-blast-off-button').length).toEqual(0)
+    })
+
+    it('Should update the `rocketRotation` and `rocketWidth` in state', (done) => {
+      childInfo.find('.rocket-blast-off-button').simulate('click')
       setTimeout(() => {
         expect(childInfo.instance().state.rocketRotation).toEqual(-45)
         expect(childInfo.instance().state.rocketWidth).toEqual(250)
-      }, 500);
+        done()
+      }, 3000);
     })
   });
 });
