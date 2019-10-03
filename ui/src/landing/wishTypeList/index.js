0112426ea5e77ab82bd55b0c9cff7a8ed0802054
Mone't Fulgham <monet_fulgham@homedepot.com>
2019-10-03 14:13:29 -0400

    Add new images to directory and update text color

--- ui/src/landing/wishTypeList/index.js
+++ ui/src/landing/wishTypeList/index.js
@@ -1,20 +1,27 @@
 import React, { Fragment } from 'react'
 import { WishType } from '../wishType'
-import Rocket from '../../assets/images/icn_To_Go_Rocket_White_Inside_130x130.png'
+import Rocket from '../../assets/images/newStyle/icon_GO_100x100.svg'
 import Alien from '../../assets/images/icn_To_Meet_Alien_White_Inside_130x130.png'
 import Astronaut from '../../assets/images/icn_To_Be_Astronaut_White_Inside_130x130.png'
 import Telescope from '../../assets/images/icn_To_See_Telescope_White_Inside_130x130.png'
 import { WishTypeCard } from '../wishTypeCard'
 import './styles.scss'
 
+const styles = {
+  text:{
+    color: '#000000'
+  }
+
+}
+
 export const WishTypeList = ({ selectWishType }) => {
   const { GO, MEET, BE, HAVE } = WishType
 
   return (
     <Fragment>
-      <h1>I wish to:</h1>
-      <ul id="WishTypeList" className="wish-type-select">
-        <li data-test="wishcard-rocket" onClick={() => selectWishType(GO)}>
+      <h1 style={styles.text}>I wish to:</h1>
+      <ul id="WishTypeList" className="wish-type-select" style={styles.text}>
+        <li data-test="wishcard-rocket" style={styles.text} onClick={() => selectWishType(GO)}>
           <WishTypeCard
             altText="Rocket"
             imgSrc={Rocket}
