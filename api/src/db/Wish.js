42199b4c149c022ff0b318853b23d5e59fea0ea0
Wes Coderre <wesley.coderre@gmail.com>
2019-10-12 00:00:50 -0700

    Added name required validation
    updated regex to include hyphens

--- api/src/db/Wish.js
+++ api/src/db/Wish.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose')
-const regex = /[{}+=_0-9\-\\|\[\])(*&^%%$#@!~`<>,?'";:]/;
+const regex = /[{}+=_0-9\\|\[\])(*&^%%$#@!~`<>,?'";:]/;
 
 const Schema = mongoose.Schema
 
 const wishSchema = new Schema(
   {
     child: {
-      name: { type: String, validate: [
-        function(name){
-          return (!regex.test(name));
-        }, 'Name must contain only letters of the alphabet'
-      ]},
+      name: { type: String,
+        validate: [
+          function(name){
+            return (!regex.test(name));
+          }, 'Name must contain only letters of the alphabet'
+        ],
+        required: [true, 'Name required']
+        },
       hometown: String,
       illness: String,
       age: Number
@@ -18,11 +21,14 @@ const wishSchema = new Schema(
     type: { type: String, enum: ['go', 'meet', 'be', 'have'] },
     details: String,
     sponsor: {
-      name: { type: String, validate: [
-        function(name){
-          return (!regex.test(name));
-        }, 'Name must contain only letters of the alphabet'
-      ]},
+      name: { type: String,
+        validate: [
+          function(name){
+            return (!regex.test(name));
+          }, 'Name must contain only letters of the alphabet'
+        ],
+        required: [true, 'Name required']
+        },
       logo: String,
       links: [String]
     }
