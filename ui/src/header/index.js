d8a8e3b8c1dba509c2c6d30ff341136feb91cdd0
Cody Stamps <crstamps2@gmail.com>
2019-10-18 09:53:21 -0400

    remove galaxy logo per new design direction
    make MAW logo larger

--- ui/src/header/index.js
+++ ui/src/header/index.js
@@ -1,26 +1,15 @@
 import React from 'react'
 import MakeAWishLogo from '../assets/images/MAW_Georgia_white.png'
-import GalaxyLogo from '../assets/images/newStyle/logo_galaxy_501x176.svg'
 import './styles.scss'
 
 export const Header = () => (
   <header>
-    <section className="slidingVertical header-item" style={{color: 'white'}}>
-      <span>Hello!</span>
-      <span>Bonjour!</span>
-      <span>Hola!</span>
-      <span>你好</span>
-      <span>こんにちは</span>
-    </section>
-    <section className="header-item">
+    <div className="header-item">
       <img
         className="header-make-a-wish-logo"
         src={MakeAWishLogo}
         alt="Make a Wish"
       />
-    </section>
-    <section className="header-item">
-      <img className="header-galaxy-logo" src={GalaxyLogo} alt="Galaxy Logo" />
-    </section>
+    </div>
   </header>
 )
