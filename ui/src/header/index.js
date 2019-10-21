a1f98b726e52e210d9467d5a3a82a85663e2e204
Cody Stamps <crstamps2@gmail.com>
2019-10-21 08:55:46 -0400

    add the sky is the limit logo
    readd 3 section header.

--- ui/src/header/index.js
+++ ui/src/header/index.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import MakeAWishLogo from '../assets/images/MAW_Georgia_white.png'
+import RocketLogo from '../assets/images/TheSkyisTheLimitLogo.png'
 import './styles.scss'
 
 export const Header = () => (
   <header>
-    <div className="header-item">
+    <section className="slidingVertical header-item" style={{color: 'white'}}>
+      <span>Hello!</span>
+      <span>Bonjour!</span>
+      <span>Hola!</span>
+      <span>你好</span>
+      <span>こんにちは</span>
+    </section>
+    <section className="header-item">
       <img
         className="header-make-a-wish-logo"
         src={MakeAWishLogo}
         alt="Make a Wish"
       />
-    </div>
+    </section>
+    <section className="header-item">
+      <img className="header-rocket-logo" src={RocketLogo} alt="Rocket Logo" />
+    </section>
   </header>
 )
