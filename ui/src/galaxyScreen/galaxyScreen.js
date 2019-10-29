f7a6557d5af5a982f6fcd2211475a95d911827a2
Cody Stamps <crstamps2@gmail.com>
2019-10-29 13:01:31 -0400

    fix naming issues

--- ui/src/galaxyScreen/galaxyScreen.js
+++ ui/src/galaxyScreen/galaxyScreen.js
@@ -3,15 +3,15 @@ import './styles.scss'
 import { getWishes } from '../services/WishDetailsService'
 import backgroundWebm from '../assets/gifs/MAW_BG.webm'
 import rocketWebm from '../assets/gifs/MAW_Rocket.webm'
-import toBeWebM from '../assets/gifs/MAW_To_Be.webm'
-import toMeetWebM from '../assets/gifs/MAW_To_Meet.webm'
+import toBeWebm from '../assets/gifs/MAW_To_Be.webm'
+import toMeetWebm from '../assets/gifs/MAW_To_Meet.webm'
 
 export default class GalaxyScreen extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            currentGif: null,
+            currentWebm: null,
             previousWishList: [],
             gifLookup: {}
         }
@@ -19,23 +19,23 @@ export default class GalaxyScreen extends Component {
 
     componentDidMount() {
         this.setState({
-            currentGif: 'MAW_BG.webm',
+            currentWebm: 'MAW_BG.webm',
             gifLookup: {
-              'MAW_BG.webm': backgroundGif,
-              'MAW_Rocket.webm': rocketGif,
-              'MAW_To_Be.webm': toBeGif,
-              'MAW_To_Meet.webm': toMeetGif
+              'MAW_BG.webm': backgroundWebm,
+              'MAW_Rocket.webm': rocketWebm,
+              'MAW_To_Be.webm': toBeWebm,
+              'MAW_To_Meet.webm': toMeetWebm
             }
         },
         () => {
             setInterval(() => {
-                this.handleCurrentGif()
+                this.handleCurrentWebm()
             },
             3000)
         })
     }
 
-    handleCurrentGif = async () => {
+    handleCurrentWebm = async () => {
         const wishes = await getWishes()
         if (JSON.stringify(this.state.previousWishList) !== JSON.stringify(wishes)) {
             const difference = wishes.filter(wish => !this.state.previousWishList.some(prevWish => wish._id === prevWish._id));
@@ -44,44 +44,44 @@ export default class GalaxyScreen extends Component {
 
                 if (difference[0].type === 'go') {
                     this.setState({
-                        currentGif: 'MAW_Rocket.webm' // 13 seconds
+                        currentWebm: 'MAW_Rocket.webm' // 13 seconds
                     }, () => {
                         setTimeout(() => {
                             this.setState({
-                                currentGif: 'MAW_BG.webm'
+                                currentWebm: 'MAW_BG.webm'
                             })
                         },
                         13000)
                     })
                 } else if (difference[0].type === 'meet') {
                     this.setState({
-                        currentGif: 'MAW_To_Meet.webm' // 11 seconds
+                        currentWebm: 'MAW_To_Meet.webm' // 11 seconds
                     }, () => {
                         setTimeout(() => {
                             this.setState({
-                                currentGif: 'MAW_BG.webm'
+                                currentWebm: 'MAW_BG.webm'
                             })
                         },
                         11000)
                     })
                 } else if (difference[0].type === 'be') {
                     this.setState({
-                        currentGif: 'MAW_To_Be.webm' // 11 seconds
+                        currentWebm: 'MAW_To_Be.webm' // 11 seconds
                     }, () => {
                         setTimeout(() => {
                             this.setState({
-                                currentGif: 'MAW_BG.webm'
+                                currentWebm: 'MAW_BG.webm'
                             })
                         },
                         11000)
                     })
                 } else {
                     this.setState({
-                        currentGif: 'MAW_Rocket.webm' // 13 seconds
+                        currentWebm: 'MAW_Rocket.webm' // 13 seconds
                     }, () => {
                         setTimeout(() => {
                             this.setState({
-                                currentGif: 'MAW_BG.webm'
+                                currentWebm: 'MAW_BG.webm'
                             })
                         },
                         13000)
@@ -89,7 +89,7 @@ export default class GalaxyScreen extends Component {
                 }
             } else {
                 this.setState({
-                    currentGif: 'MAW_BG.webm'
+                    currentWebm: 'MAW_BG.webm'
                 })
             }
 
@@ -101,16 +101,16 @@ export default class GalaxyScreen extends Component {
 
     getSourceURL = () => {
         if(process.env.REACT_APP_imageUrl) {
-            return process.env.REACT_APP_imageUrl + this.state.currentGif
+            return process.env.REACT_APP_imageUrl + this.state.currentWebm
         }
-        return this.state.gifLookup[this.state.currentGif]
+        return this.state.gifLookup[this.state.currentWebm]
     }
 
     render() {
         return (
             <div className="fullscreen-bg">
                 <video loop muted autoPlay className="fullscreen-bg__video">
-                    <source src={this.state.currentGif} type="video/webm" />
+                    <source src={this.state.currentWebm} type="video/webm" />
                 </video>
             </div>
         )
